9df6b2a52e0203ee33044fa89a1e3c8fe8008f83
Leach Wyatt <830933@stma.k12.mn.us>
2025-09-29 14:06:25 -0500

    SimpleMadLib WIP

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,23 @@
+#woah :O
+import os
+
+os.system ('cls')
+# Once upon a time, in a [adjective] [noun],
+# there lived a [adjective] [noun].
+# Every day, the [noun] would [verb] [adverb].
+# One day, a [adjective] [noun] appeared
+# and [verb] the [noun] away.
+# The [noun] was [adjective] and [emotion].
+
+timeAdjective = input("Once upon a time, in a...\nadjective: ")
+os.system ('cls')
+timeNoun = input(f"Once upon a time, in a {timeAdjective}...\nnoun: ")
+os.system ('cls')
+livedAdjective = input("there lived a...\nadjective:")
+os.system ('cls')
+livedNoun = input(f"there lived a {livedAdjective}\nnoun:")
+os.system ('cls')
+dayNoun = input(f"Every day, the {livedNoun} would...\nverb:")
+os.system ('cls')
+
+
---
+++ Practice/DataConversions.py
@@ -0,0 +1,29 @@
+# Start by copying this code block into a new file called dataconversion.py in your unit 2 folder. This is an ungraded activity.
+
+# Successfully convert all of the following variables to another type and print the result
+# If the conversion prints without errors, you did the conversion correctly
+
+a = 115         #int -> string
+print(str(a))
+b = 3.14        #float -> string
+print(str(b))
+c = "68"        #string -> int
+print(int(c))
+d = "True"      #string -> boolean
+print(bool(d))
+e = True        #boolean -> string
+print(str(e))
+f = False       #boolean -> string
+print(str(f))
+g = '10110111'  #byte -> int
+print(int(g))
+h = "2.54"      #string -> float
+print(float(h))
+i = 100         #int -> float
+print(float(i))
+j = 10.0        #float -> int
+print(int(j))
+k = 254         #int -> byte
+print(bin(k))
+
+#i did it!!! :D 
