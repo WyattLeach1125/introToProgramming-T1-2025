8f93b47dbd620d57c9dc47043a9ab815655723ff
Leach Wyatt <830933@stma.k12.mn.us>
2025-09-30 13:47:04 -0500

    Updated SimpleMadLib

--- Assignments/SimpleMadLib.py
+++ Assignments/SimpleMadLib.py
@@ -9,15 +9,39 @@ os.system ('cls')
 # and [verb] the [noun] away.
 # The [noun] was [adjective] and [emotion].
 
-timeAdjective = input("Once upon a time, in a...\nadjective: ")
-os.system ('cls')
-timeNoun = input(f"Once upon a time, in a {timeAdjective}...\nnoun: ")
-os.system ('cls')
-livedAdjective = input("there lived a...\nadjective:")
-os.system ('cls')
-livedNoun = input(f"there lived a {livedAdjective}\nnoun:")
-os.system ('cls')
-dayNoun = input(f"Every day, the {livedNoun} would...\nverb:")
-os.system ('cls')
+'''
 
+-make the user input an adj
+-make the user input a noun
+-make the user input another adj
+-make the user input another noun
+-make the user input a verb
+-make the user input an adverb
+-make the user input another adj
+-make the user input another noun
+-make the user input another verb
+-make the user input another adj
+-make the user input an emotion
+
+-print all that into the madlib format
+
+'''
+timeAdj = input("Once upon a time, in a...\nadjective: ")
+timeNoun = input(f"Once upon a time, in a {timeAdj}...\nnoun: ")
+livedAdj= input("there lived a...\nadjective: ")
+livedNoun = input(f"there lived a {livedAdj}\nnoun: ")
+everyVerb = input(f"Every day, the {livedNoun} would...\nverb: ")
+everyAdverb = input(f"Every day, the {livedNoun} would {everyVerb}...\nadverb: ")
+oneAdj = input("One day, a...\nadjective: ")
+oneNoun = input(f"One day, a {oneAdj}...\nnoun: ")
+andVerb = input("and...\nverb: ")
+# print(f"and {andVerb} the {livedNoun}")
+theAdj = input(f"The {timeNoun} was...\nadjective: ")
+theEmotion = input(f"The {timeNoun} was {theAdj} and...\nemotion: ")
+
+os.system('cls')
+
+print(f"Once upon a time, in a {timeAdj} {timeNoun}\nthere lived a {livedAdj} {livedNoun}.")
+print(f"Every day, the {timeAdj} {timeNoun}\nthere lived a {livedAdj} {livedNoun}\nEvery day, the {livedNoun} would {everyVerb} {everyAdverb}")
+print(f"One day, a {oneAdj} {oneNoun} appeared\nand {andVerb} the {livedNoun} away\nThe {timeNoun} was {theAdj} and {theEmotion}\n")
 
