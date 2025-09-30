8996a866d5991fcb158ed6ab34cd478002418bd3
Leach Wyatt <830933@stma.k12.mn.us>
2025-09-30 14:12:24 -0500

    added CalculateTax WIP

---
+++ Practice/2.6-functions.py
@@ -0,0 +1,18 @@
+#woah!!! :O
+
+#Parameters AND arguements
+
+#Create a function that adds 2 numbers together
+def add(x,y,z):
+    print(x+y+z)
+
+
+add(10,30,1)
+add(12,4,3)
+
+
+
+
+
+
+
---
+++ Practice/CalculateTax.py
@@ -0,0 +1,3 @@
+
+def calculate_tax(item,price,rate):
+    print(f"The {item} has a tax on it")
