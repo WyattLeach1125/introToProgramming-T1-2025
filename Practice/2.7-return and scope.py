892d79f49a48785a07a340cbaa9b2570785f73d1
Leach Wyatt <830933@stma.k12.mn.us>
2025-10-02 13:49:50 -0500

    return and scope

--- Practice/2.7-return and scope.py
+++ Practice/2.7-return and scope.py
@@ -1,6 +1,34 @@
 
-x = input("Insert a number: ")
-y = input("Insert a number: ")
-operation = input("What operation would you like to choose (+,-,*,/)")
+x = float(input("Insert a number for x: "))
+y = float(input("Insert a number for y: "))
 
-                  
+
+def add(x, y):
+    print(x + y)
+
+def subtract(x, y):
+    print(x - y)
+
+def multiply(x, y):
+    print(x * y)
+
+def divide(x, y):
+    print(x / y)
+
+def exponent(x, y):
+    print("exponent of x:", x * x)
+    print("exponent of y:", y * y)
+
+def modulus(x, y):
+    print("x devided by y has a remainder of:", x % y)
+
+def floor_division(x, y):
+    print(x // y)
+
+add(x, y)
+subtract(x, y)
+multiply(x, y)
+divide(x, y)
+exponent(x, y)
+modulus(x, y)
+floor_division(x, y)
