9462999cbd7d6b85a1a1587ed25811d476abec62
Leach Wyatt <830933@stma.k12.mn.us>
2025-10-06 13:53:52 -0500

    Unit2Test

--- Assignments/Unit2Test.py
+++ Assignments/Unit2Test.py
@@ -1,10 +1,10 @@
-'''
+
 #1
 word1 = input("give a random word: ")
 word2 = input("give another random word: ")
 word3 = input("give one more random word: ")
 print(word1, word2, word3)
-'''
+
 #2
 int1 = int(input("\ngive a random integer: "))
 int2 = int(input("give another integer: "))
